301e6d578d5d544be2d5df38fc2681b9c65becb4
ashishyadav83 <ashish@syncarc.net>
2024-08-30 14:32:01 +0530

    initial commit1

---
+++ example/instance-tamplete/main.tf
@@ -0,0 +1,93 @@
+provider "google" {
+  project = "local-concord-408802"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+#####==============================================================================
+##### vpc module call.
+#####==============================================================================
+module "vpc" {
+  source                                    = "SyncArcs/vpc/google"
+  version                                   = "1.0.1"
+  name                                      = "app"
+  environment                               = "test"
+  routing_mode                              = "REGIONAL"
+  network_firewall_policy_enforcement_order = "AFTER_CLASSIC_FIREWALL"
+}
+
+#####==============================================================================
+##### subnet module call.
+#####==============================================================================
+module "subnet" {
+  source        = "SyncArcs/subnet/google"
+  version       = "1.0.1"
+  name          = "app"
+  environment   = "test"
+  subnet_names  = ["subnet-a"]
+  gcp_region    = "asia-northeast1"
+  network       = module.vpc.vpc_id
+  ip_cidr_range = ["10.10.1.0/24"]
+}
+
+#####==============================================================================
+##### firewall module call.
+#####==============================================================================
+module "firewall" {
+  source        = "SyncArcs/firewall/google"
+  version       = "1.0.1"
+  name          = "app"
+  environment   = "test"
+  network       = module.vpc.vpc_id
+  source_ranges = ["0.0.0.0/0"]
+
+  allow = [
+    { protocol = "tcp"
+      ports    = ["22", "80"]
+    }
+  ]
+}
+
+#####==============================================================================
+##### instance_template module call.
+#####==============================================================================
+
+module "instance_template" {
+  source               = "../../"
+  name                 = "template"
+  environment          = "test"
+  region               = "asia-northeast1"
+  source_image         = "ubuntu-2204-jammy-v20230908"
+  source_image_family  = "ubuntu-2204-lts"
+  source_image_project = "ubuntu-os-cloud"
+  disk_size_gb         = "20"
+  subnetwork           = module.subnet.subnet_id
+  instance_template    = true
+  service_account      = null
+  ## public IP if enable_public_ip is true
+  enable_public_ip = true
+  metadata = {
+    ssh-keys = <<EOF
+      dev:ssh-rsa AAAAB3NzaC1yc2EAA/3mwt2y+PDQMU= ashish@ashish
+    EOF
+  }
+}
+
+#####==============================================================================
+##### compute_instance module call.
+#####==============================================================================
+module "compute_instance" {
+  source                 = "../../"
+  name                   = "instance"
+  environment            = "test"
+  region                 = "asia-northeast1"
+  zone                   = "asia-northeast1-a"
+  subnetwork             = module.subnet.subnet_id
+  instance_from_template = true
+  deletion_protection    = false
+  service_account        = null
+
+  ## public IP if enable_public_ip is true
+  enable_public_ip         = true
+  source_instance_template = module.instance_template.self_link_unique
+}
